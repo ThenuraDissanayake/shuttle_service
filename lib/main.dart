29eed12d604b3d2d1e1c38ae86a56107338416ce
ThenuraDissanayake <thenurabudvin@gmail.com>
2025-05-18 17:52:04 +0530

    Add app icons and configuration service for environment variables
    - Added new app icons for different resolutions in the iOS project.
    - Implemented ConfigService to manage environment variables for Google Maps API, Firebase API, and PayHere credentials using flutter_dotenv package.

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:shuttle_service/routs.dart';
 import 'package:shuttle_service/services/notification_service.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(); // Initialize Firebase
-  await NotificationService.initialize(); // Initialize notifications
+  try {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp(); // Initialize Firebase
+    await NotificationService.initialize(); // Initialize notifications
+    await dotenv.load(fileName: ".env");
+  } catch (e) {
+    print("Error during initialization: $e");
+    // Continue without .env if it fails to load
+  }
   runApp(const MyApp());
 }
 
